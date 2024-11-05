ca397eb670d4fbfc6797950886fb826cc768c6ee
mizuirorivi <onodaha@gmail.com>
2024-11-06 02:23:35 +0900

    init

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import RPi.GPIO as GPIO
 from RPLCD import CharLCD
-lcd = CharLCD(cols=16, rows=2, pin_rs=18, pin_e=23, pins_data=[12, 16, 20, 21])
+
+lcd = CharLCD(cols=16, rows=2, pin_rs=18, pin_e=23, pins_data=[12, 16, 20, 21],
+              numbering_mode=GPIO.BCM)
 lcd.write_string(u'Hello world!')
